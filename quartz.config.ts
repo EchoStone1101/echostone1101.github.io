19f558cb641c1b778227734d64fef6eea8984f53
EchoStone <echostone1101@gmail.com>
2025-12-11 23:02:17 +0800

    Quartz sync: Dec 11, 2025, 11:02 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "echostone1101.github.io/homepage/",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
